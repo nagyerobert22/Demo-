4cae6f4f9e355423969a4fe0cf428a3897dff342
robbynagy122 <nagyesuper@gmail.com>
2022-09-21 22:42:48 +0300

    test

--- V1/Form1.Designer.cs
+++ V1/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button_start = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button_start
@@ -41,32 +40,21 @@
             this.button_start.Text = "Start";
             this.button_start.UseVisualStyleBackColor = true;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(26, 305);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(84, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_start);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button_start;
-        private TextBox textBox1;
     }
 }
--- V1/Form1.cs
+++ V1/Form1.cs
@@ -14,10 +14,5 @@ namespace V1
            
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            textBox1.Text = "SANDU E GAY";
-            textBox1.Name = "text_box_1";
-        }
     }
 }
